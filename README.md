7868c18abf5fa2a4942728222bc8cf89fb20945b
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2015-03-18 19:33:44 -0700

    initial commit with friend requests accepted automatically and shown in a listview

---
+++ README.md
@@ -0,0 +1,10 @@
+# ToxWinApp
+ToxWinApp is [Tox](https://github.com/irungentoo/ProjectTox-Core) client for windows store apps that is intended to run on tablets and desktop PCs running windows 8.1 or higher
+
+# Getting Started
+
+    git clone --recursive https://github.com/mohamedkomalo/ToxWinApp.git
+
+* you need windows 8.1 with Visual Studio 2013
+
+* get libtox.dll from [Toxy builds](https://jenkins.impy.me) add it inside the "ToxWinApp" folder
---
+++ ToxWinApp/MainPage.xaml.cs
@@ -0,0 +1,113 @@
+﻿using SharpTox.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System.UserProfile;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ToxWinApp
+{
+    public sealed partial class MainPage : Page
+    {
+        private Tox tox;
+
+        //check https://wiki.tox.im/Nodes for an up-to-date list of nodes
+        private ToxNode[] Nodes = new ToxNode[]
+        {
+            new ToxNode("178.62.250.138", 33445, new ToxKey(ToxKeyType.Public, "788236D34978D1D5BD822F0A5BEBD2C53C64CC31CD3149350EE27D4D9A2F9B6B"))
+        };
+
+        private ObservableCollection<Friend> myFriends = new ObservableCollection<Friend>();
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+            initTox();
+
+            //tox.Dispose();
+        }
+
+        public async void initTox()
+        {
+            ToxOptions options = new ToxOptions(true, false);
+
+            tox = new Tox(options);
+            
+            friendsListView.ItemsSource = myFriends;
+
+            foreach (int friendNumber in tox.FriendList)
+            {
+                Friend f = new Friend();
+                f.Name = tox.GetName(friendNumber);
+                f.Status = tox.GetStatusMessage(friendNumber);
+                myFriends.Add(f);
+            }
+            myFriends.Add(new Friend() { Name = "komalo", Status = "offline" });
+
+            tox.OnFriendRequest += tox_OnFriendRequest;
+            tox.OnFriendMessage += tox_OnFriendMessage;
+
+            foreach (ToxNode node in Nodes)
+                tox.BootstrapFromNode(node);
+
+            string displayName = await UserInformation.GetDisplayNameAsync();
+
+            tox.Name = String.IsNullOrEmpty(displayName) ? "New User" : displayName;
+
+            tox.StatusMessage = "Hello World!";
+
+            tox.Start();
+
+            string id = tox.Id.ToString();
+            myIDtxt.Text = id;
+        }
+
+        private void tox_OnFriendMessage(object sender, ToxEventArgs.FriendMessageEventArgs e)
+        {
+            //get the name associated with the friendnumber
+            string name = tox.GetName(e.FriendNumber);
+
+            //print the message to the console
+            System.Diagnostics.Debug.WriteLine("<{0}> {1}", name, e.Message);
+        }
+
+        private void tox_OnFriendRequest(object sender, ToxEventArgs.FriendRequestEventArgs e)
+        {
+            //automatically accept every friend request we receive
+            int friendNumber = tox.AddFriendNoRequest(new ToxKey(ToxKeyType.Public, e.Id));
+            
+            Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High, () =>
+            {
+                Friend f = new Friend();
+                f.Id = e.Id;
+                f.Name = tox.GetName(friendNumber);
+
+                while (String.IsNullOrEmpty(f.Name))
+                {
+                    Task.Delay(30);
+                    f.Name = tox.GetName(friendNumber);
+                }
+
+                f.Status = tox.GetStatusMessage(friendNumber);
+
+                myFriends.Add(f);
+            });
+        }
+    }
+}
